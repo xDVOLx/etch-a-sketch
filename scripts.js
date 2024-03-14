dbd04dfefcb890a30134ecc9fa7f31341e48b631
xDVOLx <156858542+xDVOLx@users.noreply.github.com>
2024-03-14 15:10:39 -0700

    change code for grid creation, add custom grid size button
    changed code for grid creation to format a bit better on mobile devices, and to eliminate unwanted extra space in grid. Also added a button under the grid for creating a new grid with a different size

--- scripts.js
+++ scripts.js
@@ -1,13 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const gridContainer = document.getElementById("grid-container");
+function makeGrid(sideLength) {
+  let gridContainer = document.getElementById("grid-container");
+  gridContainer.replaceChildren();
+  let cellHeight = Math.floor(500 / sideLength) + "px";
+  let cellWidth = Math.floor(500 / sideLength) + "px";
+  for (let i = 0; i < sideLength; i++) {
+    let column = document.createElement("div");
+    column.classList.add("column");
+    for (let j = 0; j < sideLength; j++) {
+      let row = document.createElement("div");
+      row.classList.add("row");
+      row.style.outline = "1px solid black";
+      row.style.height = cellHeight;
+      row.style.width = cellWidth;
+      row.addEventListener("mouseover", function () {
+        this.style.backgroundColor = "black";
+      });
+      column.appendChild(row);
+    }
+    gridContainer.appendChild(column);
+  }
+}
 
-  for (let i = 0; i < 16 * 16; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    gridContainer.appendChild(cell);
+const changeGridSizeButton = document.getElementById('change-grid-size');
+changeGridSizeButton.addEventListener("click", function() {
+    let gridSize = prompt("Type your desired grid size (e.g. 16 for a 16x16 grid, max: 100, grid will default to 16x16 if an error occurs");
+    let gridSizeInt = parseInt(gridSize);
+    makeGrid(gridSizeInt);
+}) 
 
-    cell.addEventListener("mouseover", function () {
-      this.style.backgroundColor = "black";
-    });
-  };
-});
+makeGrid(16);
