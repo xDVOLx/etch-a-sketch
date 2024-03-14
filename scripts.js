c0b1396e986500195214f8aaa4fb58172ec9564e
xDVOLx <156858542+xDVOLx@users.noreply.github.com>
2024-03-14 15:39:29 -0700

    add rainbow button, change page colors, add alert
    Changed page background color to d3d3d3, added button for changing hover color from black to rainbow (not yet coded), added alert message for change grid size if the function cannot parse the input correctly

--- scripts.js
+++ scripts.js
@@ -1,31 +1,41 @@
 function makeGrid(sideLength) {
-  let gridContainer = document.getElementById("grid-container");
-  gridContainer.replaceChildren();
-  let cellHeight = Math.floor(500 / sideLength) + "px";
-  let cellWidth = Math.floor(500 / sideLength) + "px";
-  for (let i = 0; i < sideLength; i++) {
-    let column = document.createElement("div");
-    column.classList.add("column");
-    for (let j = 0; j < sideLength; j++) {
-      let row = document.createElement("div");
-      row.classList.add("row");
-      row.style.outline = "1px solid black";
-      row.style.height = cellHeight;
-      row.style.width = cellWidth;
-      row.addEventListener("mouseover", function () {
-        this.style.backgroundColor = "black";
-      });
-      column.appendChild(row);
+  if (!sideLength || isNaN(sideLength) || sideLength <= 0 || sideLength > 100) {
+    alert("Please choose a number between 1 and 100")
+  } else {
+    let gridContainer = document.getElementById("grid-container");
+    gridContainer.replaceChildren();
+    let cellHeight = Math.floor(500 / sideLength) + "px";
+    let cellWidth = Math.floor(500 / sideLength) + "px";
+    for (let i = 0; i < sideLength; i++) {
+      let column = document.createElement("div");
+      column.classList.add("column");
+      for (let j = 0; j < sideLength; j++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.style.outline = "1px solid black";
+        row.style.height = cellHeight;
+        row.style.width = cellWidth;
+        row.style.backgroundColor = "white";
+        row.addEventListener("mouseover", function () {
+          this.style.backgroundColor = "black";
+        });
+        column.appendChild(row);
+      }
+      gridContainer.appendChild(column);
     }
-    gridContainer.appendChild(column);
   }
 }
 
-const changeGridSizeButton = document.getElementById('change-grid-size');
-changeGridSizeButton.addEventListener("click", function() {
-    let gridSize = prompt("Type your desired grid size (e.g. 16 for a 16x16 grid, max: 100, grid will default to 16x16 if an error occurs");
-    let gridSizeInt = parseInt(gridSize);
-    makeGrid(gridSizeInt);
-}) 
+const changeGridSizeButton = document.getElementById("change-grid-size");
+changeGridSizeButton.addEventListener("click", function () {
+  let gridSize = prompt(
+    "Type your desired grid size (e.g. 16 for a 16x16 grid, min: 1 max: 100"
+  );
+  let gridSizeInt = parseInt(gridSize);
+  makeGrid(gridSizeInt);
+});
+
+const rainbowModeButton = document.getElementById("rainbow-mode");
+rainbowModeButton.addEventListener("click", function () {});
 
 makeGrid(16);
